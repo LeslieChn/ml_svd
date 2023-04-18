81bda61cf318aa458bdf5ed52ee6142ade4544e6
kynsity <knb2139@columbia.edu>
2023-04-18 17:26:58 -0400

    added method to truncate SVD components

--- project.py
+++ project.py
@@ -37,46 +37,50 @@ print(np.shape(ATest)) #looks good
 
 #writing out svd by hand: Source MIT opencourseware https://web.mit.edu/be.400/www/SVD/Singular_Value_Decomposition.htm
 
-#find V: eigenvectors of A transpose * A
-AT_A = ATest.transpose()@ATest
-w,v = np.linalg.eig(AT_A) #w eigenvalues, v eigenvectors
-ncols = np.argsort(w)[::-1]
-
-#now, apply Gram-Schmidt transformation to make orthonormal
+#Gram-Schmidt transformation to make orthonormal
 def gram_schmidt(X):
-    Q, R = np.linalg.qr(X) #can i take this shortcut?
-    return Q
+  Q, R = np.linalg.qr(X)
+  return Q
 
-v = v[:,ncols] #sort descending
-V = gram_schmidt(v).transpose() #now V is orthonormal eigenvectors of A transpose * A
+def SVD(A):
+  #find V: eigenvectors of A transpose * A
+  AT_A = A.T@A
+  w,v = np.linalg.eig(AT_A) #w eigenvalues, v eigenvectors
+  ncols = np.argsort(w)[::-1]
 
+  v = v[:,ncols] #sort descending
+  V = gram_schmidt(v).T #now V is orthonormal eigenvectors of A transpose * A
 
-#Sigma: diagonal matrix whose entries are the sqrt of eigenvalues of Atranspose*A / A*Atranspose
+  #Sigma: diagonal matrix whose entries are the sqrt of eigenvalues of Atranspose*A / A*Atranspose
 
-roots = np.sqrt(np.abs(w)) #what to do if eig vals are negative?
-sorted = np.sort(roots)[::-1] #sort in descending order
-Sigma = np.diag(sorted, k=0) #k = 0 specifices main diagonal, square roots of w
+  roots = np.sqrt(np.abs(w)) #what to do if eig vals are negative?
+  sorted = np.sort(roots)[::-1] #sort in descending order
+  Sigma = np.diag(sorted, k=0) #k = 0 specifices main diagonal, square roots of w
 
-#U: uniquely determined by V. Ui = Vi@A/sigmai
-i = 0
-m = np.zeros(10)
-for x in V:
-  y = 1/(sorted[i])*(ATest@x)
-  if(i == 0):
-    m = y
-  else:
-    m = np.c_[m, y]
-  i+=1
+  #U: uniquely determined by V. Ui = Vi@A/sigmai
+  i = 0
+  m = np.zeros(10)
+  for x in V:
+    y = 1/(sorted[i])*(ATest@x)
+    if(i == 0):
+      m = y
+    else:
+      m = np.c_[m, y]
+    i+=1
 
-U = m
+  U = m
+  return U, Sigma, V
 
-#now,theoretically, have all three components, let's try to reconstruct:
+#if Anxm : U nxn, Sigma nxm, V mxm
+#can reduce Sigma to c greatest singular vals (Symeonidis P. Matrix and Tensor Factorization Tech for Recommender Systems 2017 p 38)
+#so: Anxm : U nxc, Sigma cxc, V cxm
+#how to choose c? book: maintain 83% of original vals
 
-ATestrec = U@Sigma@V
+def truncate(U, S, V, c): #truncates U,S,Vt to nxc, cxc, cxm
+    UNew = U[:U.shape[0],:c]
+    SNew = S[:c,:c]
+    VNew = V[:c,:V.shape[1]]
+    return UNew, SNew, VNew
 
-print(ATest)
-print(ATestrec)
-
-#for idx, x in np.ndenumerate(ATest):
-  #if(x != ATestrec[idx]):
-     # print(idx, x, ATestrec[idx])
+#u1,s1,v1 = SVD(A)
+#print(truncate(u1,s1,v1,30))
